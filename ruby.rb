0cc921c4be62e67b8c8491a2998c76cd523f67c5
IvanPagani <ivanpagani07@gmail.com>
2025-03-16 11:45:54 -0300

    Minor changes to the code readability

--- ruby.rb
+++ ruby.rb
@@ -1,26 +1,27 @@
-def stock_picker(stock_prices)
+def stock_picker(stockPrices)
+  
+  maxProfit = 0
+  bestDays = []
+  
+  stockPrices[0..-2].each_with_index do |buyPrice, buyDay|
     
-    maxProfit = 0
-    bestDays = Array.new
-
-    stock_prices[0..-2].each_with_index do |buy, indexBuy|
-
-        # this is a little tricky. This second loop only iterates ahead of where the previous loop is
-        # however, indexSell's value always starts at 0. That's why it is required to add it to (indexBuy+1)
-        stock_prices[(indexBuy+1)..-1].each_with_index do |sell, indexSell|
-            
-            if (sell - buy) > maxProfit
-                maxProfit = (sell - buy)
-                bestDays = [[indexBuy, ((indexBuy+1) + indexSell)]]
-                p bestDays
-            elsif (sell - buy) == maxProfit
-                bestDays.push([indexBuy, ((indexBuy+1) + indexSell)])
-                p bestDays
-            end
-
-        end
+    # This one is a little tricky. This loop only iterates ahead of where the previous loop is
+    # However, sellDay's value always starts at 0. That's why it is required to add (buyDay+1)
+    stockPrices[(buyDay+1)..-1].each_with_index do |sellPrice, sellDay|
+      profit = (sellPrice - buyPrice)
+      
+      if profit > maxProfit
+        maxProfit = profit
+        bestDays = [[buyDay, ((buyDay+1) + sellDay)]]
+      elsif profit == maxProfit
+        bestDays.push([buyDay, ((buyDay+1) + sellDay)])
+      end
+    
     end
-    return bestDays
+  end
+
+  return bestDays
 end
 
-stock_picker([17,6,3,9,15,8,6,15,1,10,13])
+#stock_picker([17,6,3,9,15,8,6,15,1,10,13])
+stock_picker([17,8,8,13,9,8,10,8,13,8,10]) # return ALL POSSIBILITIES
